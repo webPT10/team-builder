bdeee01788dc3365b9de07a912ffc62ad97b8b08
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-11-04 11:37:50 -0800

    ADDED -- useState hook to App.js

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'; // added useState
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          This is. React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
